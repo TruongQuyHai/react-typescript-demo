766f6f9828b0efdf424b5686670bde12573ea1f0
Truong Quy Hai <haiquytruong@gmail.com>
2022-03-07 15:32:20 +0700

    first section

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
+  // const personName = {
+  //   first: "Bruce",
+  //   last: "Wayne",
+  // };
+
+  // const nameList = [
+  //   {
+  //     first: "Bruce",
+  //     last: "Wayne",
+  //   },
+  //   {
+  //     first: "Clark",
+  //     last: "Kent",
+  //   },
+  //   {
+  //     first: "Princess",
+  //     last: "Diana",
+  //   },
+  // ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet name="John Leo" messageCount={30} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} /> */}
+      <Status status="loading" />
+      <Heading>Place holder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
+      <Greet name="John Doe" isLoggedIn={true} />
     </div>
   );
 }
