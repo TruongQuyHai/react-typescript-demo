ff36af6735b23999280cdba7bdcda78b7249c639
Truong Quy Hai <haiquytruong@gmail.com>
2022-03-08 09:44:47 +0700

    Ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import "./App.css";
-import { User } from "./components/context/User";
-import { UserContextProvider } from "./components/context/UserContext";
 
 function App() {
-  return (
-    <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
